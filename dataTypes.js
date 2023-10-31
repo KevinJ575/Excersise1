78278d6f490b1351a9a09ea807e8dc6a5218249c
KevinJ575 <2202075@utrivieramaya.edu.mx>
2023-10-30 22:39:53 -0500

    first commit

---
+++ dataTypes.js
@@ -0,0 +1,36 @@
+// Verificar los tipos de datos de las variables
+console.log("Tipo de dato de cadena: " + typeof 'hola');
+console.log("Tipo de dato de booleano: " + typeof true);
+console.log("Tipo de dato de indefinido: " + typeof undefined);
+console.log("Tipo de dato de nulo: " + typeof null);
+
+
+
+// Declarar cuatro variables sin asignar valores
+let valor1, valor2, valor3, valor4;
+
+// Declarar cuatro variables con valores asignados
+let numero = 15;
+let texto = "Hola";
+let falso = false;
+let numero2 = 16;
+
+// Declarar variables para almacenar información personal en varias líneas
+let nombre = "Kevin";
+let apellido = "Reyes";
+let pais = "Mexico";
+let edad = 19;
+let estadoCivil = "Soltero";
+
+// Declarar variables para almacenar información personal en una sola línea
+let nombre2 = "Kevin", apellido2 = "Reyes",  pais2 = "México", edad2 = 19;
+
+
+// Declarar y enviar las variables myAge y yourAge a la consola del navegador
+
+let myAge= 19;
+let yourAge=45;
+console.log("myAge es:", myAge);
+console.log(yourAge);
+
+
---
+++ variables.js
@@ -0,0 +1,16 @@
+//Este es mi primer codijo Js
+
+/*Los comentarios pueden hacer que el codigo sea legible,
+facil de reutilizar e informativo.*/
+
+// Declarar variables y asignar valores de diferentes tipos de datos
+let texto1 ="hola";
+let boolean = true;
+let undefined1 = undefined;
+let number = 15;
+
+
+
+
+
+
